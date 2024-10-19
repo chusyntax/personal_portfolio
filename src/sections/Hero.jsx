6a59f6b9e6945004c4e61d3d57d030ff9db09ad0
Chu <ttheko101@gmail.com>
2024-10-19 17:43:19 +0200

    first commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,56 @@
+import { Canvas } from "@react-three/fiber";
+import HackerRoom from "../components/HackerRoom";
+import { PerspectiveCamera } from "@react-three/drei";
+import { Suspense } from "react";
+import CanvasLoader from "../components/CanvasLoader";
+//import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants/index.js";
+import Target from "../components/Target.jsx";
+//import Playstation5 from "../components/ReactLogo.jsx";
+import ReactLogo from "../components/ReactLogo.jsx";
+import Cube from "../components/Cube.jsx";
+import Rings from "../components/Rings.jsx";
+const Hero = () => {
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 gap-3 c-space">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+          Hey there, I&apos;m Chu <span className="waving-hand">👋🏾</span>
+        </p>
+
+        <p className="sm:text-xl text-lg font-medium text-gray-400 text-center">
+          Software Engineer | Full Stack Developer
+        </p>
+      </div>
+      <div className="w-full h-full absolute inset-0">
+        {/* <Leva /> */}
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom
+              position={sizes.deskPosition}
+              rotation={[0.2, 0, 0]}
+              scale={sizes.deskScale}
+            />
+            <group>
+              <Target position={sizes.targetPosition} />
+              <ReactLogo position={sizes.reactLogoPosition} />
+              <Cube position={sizes.cubePosition} />
+              <Rings position={sizes.ringPosition} />
+            </group>
+            <ambientLight intensity={2.5} />
+            <directionalLight position={[10, 10, 10]} intensity={2.5} />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
