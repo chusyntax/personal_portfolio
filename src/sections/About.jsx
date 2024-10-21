a5c9dd48ca977564973d1f65b535eff48f8b5127
Chu <ttheko101@gmail.com>
2024-10-21 23:08:26 +0200

    Initial Projects section completed

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   const [hasCopied, setHasCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(" adrian@jsmastery.pro");
+    navigator.clipboard.writeText("ttheko101@gmail.com");
     setHasCopied(true);
 
     setTimeout(() => {
